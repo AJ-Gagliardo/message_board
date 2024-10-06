cdbabfb360f4cc8e40d9013ea69a2b49ed207ed7
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-10-05 22:11:38 -0700

    just creating the port and basic express structure

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const express = require("express");
 const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
+
+app.listen(port, () => {
+  console.log(`listening on Port: ${port}`);
+});
+
+app.get("*", (req, res) => {
+  res.send("Error 404, Page not found");
+});
+
+app.post("/", (req, res) => {
+  console.log("this will send a post request");
+});
