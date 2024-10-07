b4a05baae4c223c4558aa6fdde27b6904cad071c
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-10-06 22:09:53 -0700

    got to make the app.post work! also made it so the id gets automatically added (instead of manually added), now I have to check how to do a UI to amke it easier for the user

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 const port = 3000;
+let quotes = JSON.parse(fs.readFileSync("./quotes.json"));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Homepage");
+  res.send(`${quotes.length}`);
+});
+app.get("/api", (req, res) => {
+  res.status(200).json({ count: quotes.length, data: { quotes: quotes } });
+  // .send("Homepage");
 });
 
 app.listen(port, () => {
@@ -15,6 +23,15 @@ app.get("*", (req, res) => {
   res.send("Error 404, Page not found");
 });
 
-app.post("/", (req, res) => {
-  console.log("this will send a post request");
+app.post("/api", (req, res) => {
+  // console.log(req.body);
+  const newId = quotes[quotes.length - 1].id + 1;
+  console.log(newId);
+  const newQuote = Object.assign({ id: newId }, req.body);
+  quotes.push(newQuote);
+
+  fs.writeFile("./quotes.json", JSON.stringify(quotes), (err) => {
+    res.status(201).json({ status: success, data: { movie: newMovie } });
+  });
+  res.send("creating a post");
 });
