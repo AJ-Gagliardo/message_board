625e5e8efa474b504eb56a83327e947298647691
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-10-31 13:28:55 -0700

    made changes to be able to upload it on railway

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const fs = require("fs");
 require("dotenv").config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+console.log(port);
 
 // to read the quotes file
 let quotes = JSON.parse(fs.readFileSync("./quotes.json"));
